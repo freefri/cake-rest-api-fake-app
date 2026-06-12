21a96163c81de1599fc083399d9ff898126a8a7e
Adrian <freefri@freefri.es>
2026-06-12 10:14:58 +0200

    PRD-11339 change deprecated AbstractMigration with newer BaseMigration

--- config/Migrations/20220113094521_CreateUsers.php
+++ config/Migrations/20220113094521_CreateUsers.php
@@ -2,9 +2,9 @@
 
 declare(strict_types = 1);
 
-use Migrations\AbstractMigration;
+use Migrations\BaseMigration;
 
-class CreateUsers extends AbstractMigration
+class CreateUsers extends BaseMigration
 {
     public function change()
     {
